a98e17248d3d9b69fb117ca4f3683d4449c5005b
sivamaari <106659878+sivamaari@users.noreply.github.com>
2022-08-21 20:39:41 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner sc=new Scanner (System.in);         int n;         n=sc.nextInt();         double d=Math.sqrt(n);         if((d-(int)d)>0)         System.out.println("False");         else         System.out.println("True");     } }

---
+++ perfect_square_root_or_not.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner (System.in);
+        int n;
+        n=sc.nextInt();
+        double d=Math.sqrt(n);
+        if((d-(int)d)>0)
+        System.out.println("False");
+        else
+        System.out.println("True");
+    }
+}
