bd41b855e98bd5b9e718c0bf75957befd94b4af8
sivamaari <106659878+sivamaari@users.noreply.github.com>
2022-10-23 16:34:44 +0530

    import java.util.*; class main {     public static int reverse(int n)     {         int rev=0,r;         while(n>0)         {             r=n%10;             rev=(rev*10)+r;             n=n/10;         }         return rev;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         System.out.println(reverse(n));     } }

--- reverse_of_a_numbers.java
+++ reverse_of_a_numbers.java
@@ -1,17 +1,21 @@
 import java.util.*;
 class main
 {
+    public static int reverse(int n)
+    {
+        int rev=0,r;
+        while(n>0)
+        {
+            r=n%10;
+            rev=(rev*10)+r;
+            n=n/10;
+        }
+        return rev;
+    }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,sum=0,r;
-        n=sc.nextInt();
-        while (n>0)
-        {
-            r=n%10;
-            sum=sum*10+r;
-            n=n/10;
-        }
-        System.out.println(sum);
+        int n=sc.nextInt();
+        System.out.println(reverse(n));
     }
 }
