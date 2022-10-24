7396e608b9976af87ce66c572f738db71a763ed3
sivamaari <106659878+sivamaari@users.noreply.github.com>
2022-10-24 17:47:56 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],e=0,p=0;         n=sc.nextInt();         x=new int[n];         for (int i=0;i<n;i++)         x[i]=sc.nextInt();         for(int i=0;i<n;i++)         {             if(x[i]%2==0)             {                 e=x[i];             }         }             for(int i=0;i<n;i++)             {                 if(x[i]==e)                 {                     p=i;                 }             }             System.out.println(p);     } }

---
+++ Last_even_element_index.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],e=0,p=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for (int i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(x[i]%2==0)
+            {
+                e=x[i];
+            }
+        }
+            for(int i=0;i<n;i++)
+            {
+                if(x[i]==e)
+                {
+                    p=i;
+                }
+            }
+            System.out.println(p);
+    }
+}
